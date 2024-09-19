335e45bdd1eaaff0f0530a487c262a6c5c0b7ccf
joep-duwel <302287059@student.rocmondriaan.nl>
2024-09-19 12:29:24 +0200

    les d3 klaar

--- les3/dbconnect.php
+++ les3/dbconnect.php
@@ -1,8 +1,24 @@
 <?php
 try {
-
-    $db=new PDO("mysql:host=localhost;dbname=cars", "root","");
-} catch (PDOException $e){
-    die("Error!:" .$e->getMessage());
+    $db = new PDO("mysql:host=localhost;dbname=cars", "root", "");
+} catch (PDOException $e) {
+    die("Error!: " . $e->getMessage());
 }
+$query = $db->prepare("SELECT * FROM electric_car");
+$query->execute();
+
+
+$cars = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<pre>";
+var_dump($cars);
+echo "</pre>";
+
+
+foreach ($cars as $car) {
+    echo $car['name'] . " ";
+    echo $car['range'] . " ";
+    echo $car['prijs'] . "<br>";
+}
+
 ?>
--- les3/index.php
+++ les3/index.php
@@ -1,2 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Electric Cars</title>
+</head>
+<body>
 <?php
-    echo "hoi";
+try {
+    // Database connection
+    $db = new PDO("mysql:host=localhost;dbname=cars", "root", "");
+} catch (PDOException $e) {
+    die("Error!: " . $e->getMessage());
+}
+
+// Query execution
+$query = $db->prepare("SELECT * FROM electric_car");
+$query->execute();
+
+// Fetch results
+$cars = $query->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!-- Table to display car data -->
+<table border="1" cellpadding="5" cellspacing="0">
+    <tr>
+        <th>Name</th>
+        <th>Range</th>
+        <th>Price</th>
+    </tr>
+    <?php foreach ($cars as $car): ?>
+        <tr>
+            <td><?= htmlspecialchars($car['name']) ?></td>
+            <td><?= htmlspecialchars($car['range']) ?></td>
+            <td><?= htmlspecialchars($car['prijs']) ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
+</body>
+</html>
